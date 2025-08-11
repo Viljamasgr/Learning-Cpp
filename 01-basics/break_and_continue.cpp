7a6c457eff135b29ca604dd179bff3c23984bc68
Viljamas Grumadas <viljamasgr@gmail.com>
2025-08-11 13:14:34 +0100

    Completed Lessons
    Completed lessons on breaking or continuing a loop. Completed a lesson on random number generation and a way to integrate it into events using a switch case.

---
+++ 01-basics/break_and_continue.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+
+    // break = break out of a loop
+    // continue = skip current iteration
+
+    for(int i = 1; i <= 20; i++){
+        if (i == 13){
+            continue;
+        }
+        std::cout << i << '\n';
+        if (i == 18){
+            break;
+        }
+    }
+
+    return 0;
+}
