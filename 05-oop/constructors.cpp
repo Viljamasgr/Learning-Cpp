39e5c1acb07425b4812e90fe9cc0d2acb2d423e0
Viljamas Grumadas <viljamasgr@gmail.com>
2025-08-26 21:39:53 +0100

    Add examples for enums and basic OOP in C++
    Added enums.cpp demonstrating enum usage and switch statements. Added constructors.cpp with comments introducing constructors. Added object_oriented_programming.cpp with a Car class example, showing attributes, methods, and object instantiation.

---
+++ 05-oop/constructors.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(){
+
+    // constructor = special method that is automatically called when an object is instantiated
+    //               useful for assigning values to attributes as arguments
+
+    return 0;
+}
